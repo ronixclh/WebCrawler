4599c9ce45a9d571c058ad40f3bffed921c40229
ronixclh <ronalds.borisovs@inbox.lv>
2024-08-25 14:18:00 +0200

    Adding main logic of Web Crawler.

---
+++ Models/NewsEntry.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WebCrawler.Models
+{
+    public class NewsEntry
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public int Points { get; set; }
+        public int Comments { get; set; }
+    }
+}
