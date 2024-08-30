81831613f9780eda560d33e0359ec945d0ccb038
ronixclh <ronalds.borisovs@inbox.lv>
2024-08-30 09:41:36 +0200

    adding configs for ports

--- WebCrawlerAPI/Program.cs
+++ WebCrawlerAPI/Program.cs
@@ -16,7 +16,12 @@ namespace WebCrawlerAPI
                 {
                     webBuilder.UseKestrel(options =>
                     {
-                        options.ListenAnyIP(80);  // Only listen for HTTP
+                        options.ListenAnyIP(80);  
+                        options.ListenAnyIP(5209);  
+                        options.ListenAnyIP(7089, listenOptions =>
+                        {
+                            listenOptions.UseHttps(); 
+                        });
                     });
                     webBuilder.UseStartup<Startup>();
                    
